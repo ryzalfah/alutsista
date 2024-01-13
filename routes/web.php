69a2c7e368e5120eef61f5568b59bf1e5889d82c
fymrym <21102045@ittelkom-pwt.ac.id>
2024-01-13 09:27:55 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,17 @@ Route::get('/', function () {
 Route::get('/login', [AdminController::class,'index'])->name('auth.index');
 Route::get('/logout', [AdminController::class,'logout'])->name('auth.logout');
 Route::post('/login', [AdminController::class,'process'])->name('auth.process');
+
 // Senjata Controller
 Route::get('/senjata', [SenjataController::class,'index'])->name('senjata.senjata');
 Route::get('/senjata/input', [SenjataController::class,'create'])->name('senjata.input');
 Route::post('/senjata', [SenjataController::class,'store'])->name('senjata.store');
 Route::get('/senjata/{senjata}', [SenjataController::class,'show'])->name('senjata.show');
+
 // Kendaraan Controller
 Route::get('/Kendaraan', [KendaraanController::class,'index'])->name('Kendaraan.Kendaraan');
 Route::get('/Kendaraan/input', [KendaraanController::class,'create'])->name('Kendaraan.input');
 Route::post('/Kendaraan', [KendaraanController::class,'store'])->name('Kendaraan.store');
 Route::get('/Kendaraan/{senjata}', [KendaraanController::class,'show'])->name('Kendaraan.show');
+
 // Pinjam Controller
